ceae9039a264c1eb0fe9c367e72888ef5718fbaa
Jeremy <jgerk2@huskers.unl.edu>
2022-08-25 19:37:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jeremy Gerk
+ *         jgerk2@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Jeremy Gerk\n");
+
+  return 0;
+}
